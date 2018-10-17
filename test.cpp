ab19a6e651cfe14c14e472017a3636b036914b90
ivanhalim <ivanhalim@users.noreply.github.com>
2018-10-16 22:12:19 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,6 +8,11 @@ int main()
 {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr)/sizeof(arr[0]);
+    for (int i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     quicksort(arr, 0, n-1);
     for (int i=0;i<n;i++)
     {
